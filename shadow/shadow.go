4b5eecfb188c3822c59d3fa31c0eaf312dfa4e2a
Atsushi Watanabe <atsushi.w@ieee.org>
2019-12-31 12:01:24 +0900

    Implement Thing Shadow

---
+++ shadow/shadow.go
@@ -0,0 +1,210 @@
+package shadow
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/at-wat/mqtt-go"
+)
+
+// Shadow is an interface of Thing Shadow.
+type Shadow interface {
+	mqtt.Handler
+	// Get thing state.
+	Get(ctx context.Context) error
+	// Report thing state.
+	Report(ctx context.Context, state interface{}) error
+	// Desire sets desired thing state.
+	Desire(ctx context.Context, state interface{}) error
+	// Document returns full thing document.
+	Document() *ThingDocument
+	// OnDelta sets handler of state deltas.
+	OnDelta(func(delta map[string]interface{}))
+	// OnError sets handler of asynchronous errors.
+	OnError(func(error))
+}
+
+type shadow struct {
+	mqtt.ServeMux
+	cli       mqtt.Client
+	thingName string
+	doc       *ThingDocument
+	mu        sync.Mutex
+	onDelta   func(delta map[string]interface{})
+	onError   func(err error)
+}
+
+func (s *shadow) topic(operation string) string {
+	return "$aws/things/" + s.thingName + "/shadow/" + operation
+}
+
+func (s *shadow) getAccepted(msg *mqtt.Message) {
+	doc := &ThingDocument{}
+	if err := json.Unmarshal(msg.Payload, doc); err != nil {
+		s.handleError(err)
+		return
+	}
+	s.doc = doc
+	s.handleDelta(s.doc.State.Delta)
+}
+
+func (s *shadow) getRejected(msg *mqtt.Message) {
+	s.handleError(fmt.Errorf("%s: %s", msg.Topic, string(msg.Payload)))
+}
+
+func (s *shadow) updateAccepted(msg *mqtt.Message) {
+	doc := &thingDocumentRaw{}
+	if err := json.Unmarshal(msg.Payload, doc); err != nil {
+		s.handleError(err)
+		return
+	}
+	if err := s.doc.update(doc); err != nil {
+		s.handleError(err)
+		return
+	}
+}
+
+func (s *shadow) updateDelta(msg *mqtt.Message) {
+	state := &thingDelta{}
+	if err := json.Unmarshal(msg.Payload, state); err != nil {
+		s.handleError(err)
+		return
+	}
+	ok, err := s.doc.updateDelta(state)
+	if err != nil {
+		s.handleError(err)
+		return
+	}
+	if ok {
+		s.handleDelta(s.doc.State.Delta)
+	}
+}
+
+func (s *shadow) updateRejected(msg *mqtt.Message) {
+	s.handleError(fmt.Errorf("%s: %s", msg.Topic, string(msg.Payload)))
+}
+
+// New creates Thing Shadow interface.
+func New(ctx context.Context, cli mqtt.Client, thingName string) (Shadow, error) {
+	s := &shadow{
+		cli:       cli,
+		thingName: thingName,
+	}
+	for _, sub := range []struct {
+		topic   string
+		handler mqtt.Handler
+	}{
+		{s.topic("update/delta"), mqtt.HandlerFunc(s.updateDelta)},
+		{s.topic("update/accepted"), mqtt.HandlerFunc(s.updateAccepted)},
+		{s.topic("update/rejected"), mqtt.HandlerFunc(s.updateRejected)},
+		{s.topic("get/accepted"), mqtt.HandlerFunc(s.getAccepted)},
+		{s.topic("get/rejected"), mqtt.HandlerFunc(s.getRejected)},
+	} {
+		if err := s.ServeMux.Handle(sub.topic, sub.handler); err != nil {
+			return nil, err
+		}
+	}
+
+	err := cli.Subscribe(ctx,
+		mqtt.Subscription{Topic: s.topic("update/delta"), QoS: mqtt.QoS1},
+		mqtt.Subscription{Topic: s.topic("update/accepted"), QoS: mqtt.QoS1},
+		mqtt.Subscription{Topic: s.topic("update/rejected"), QoS: mqtt.QoS1},
+		mqtt.Subscription{Topic: s.topic("get/accepted"), QoS: mqtt.QoS1},
+		mqtt.Subscription{Topic: s.topic("get/rejected"), QoS: mqtt.QoS1},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func (s *shadow) Report(ctx context.Context, state interface{}) error {
+	rawState, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+	rawStateJSON := json.RawMessage(rawState)
+	data, err := json.Marshal(&thingDocumentRaw{
+		State: thingStateRaw{Reported: rawStateJSON},
+	})
+	if err != nil {
+		return err
+	}
+	if err := s.cli.Publish(ctx, &mqtt.Message{
+		Topic:   s.topic("update"),
+		QoS:     mqtt.QoS1,
+		Payload: data,
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *shadow) Desire(ctx context.Context, state interface{}) error {
+	rawState, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+	rawStateJSON := json.RawMessage(rawState)
+	data, err := json.Marshal(&thingDocumentRaw{
+		State: thingStateRaw{Desired: rawStateJSON},
+	})
+	if err != nil {
+		return err
+	}
+	if err := s.cli.Publish(ctx, &mqtt.Message{
+		Topic:   s.topic("update"),
+		QoS:     mqtt.QoS1,
+		Payload: data,
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *shadow) Get(ctx context.Context) error {
+	if err := s.cli.Publish(ctx, &mqtt.Message{
+		Topic:   s.topic("get"),
+		QoS:     mqtt.QoS1,
+		Payload: []byte{},
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *shadow) Document() *ThingDocument {
+	return s.doc
+}
+
+func (s *shadow) OnDelta(cb func(delta map[string]interface{})) {
+	s.mu.Lock()
+	s.onDelta = cb
+	s.mu.Unlock()
+}
+
+func (s *shadow) handleDelta(delta map[string]interface{}) {
+	s.mu.Lock()
+	cb := s.onDelta
+	s.mu.Unlock()
+	if cb != nil {
+		cb(delta)
+	}
+}
+
+func (s *shadow) OnError(cb func(err error)) {
+	s.mu.Lock()
+	s.onError = cb
+	s.mu.Unlock()
+}
+
+func (s *shadow) handleError(err error) {
+	s.mu.Lock()
+	cb := s.onError
+	s.mu.Unlock()
+	if cb != nil {
+		cb(err)
+	}
+}
---
+++ shadow/state.go
@@ -0,0 +1,122 @@
+package shadow
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// ErrVersionConflict means thing state update was aborted due to version conflict.
+var ErrVersionConflict = errors.New("version conflict")
+
+// ThingState represents Thing Shadow State.
+type ThingState struct {
+	Desired  map[string]interface{} `json:"desired,omitempty"`
+	Reported map[string]interface{} `json:"reported,omitempty"`
+	Delta    map[string]interface{} `json:"delta,omitempty"`
+}
+
+// ThingDocument represents Thing Shadow Document.
+type ThingDocument struct {
+	State     ThingState `json:"state"`
+	Version   int        `json:"version,omitempty"`
+	Timestamp int        `json:"timestamp,omitempty"`
+}
+
+type thingStateRaw struct {
+	Desired  json.RawMessage `json:"desired,omitempty"`
+	Reported json.RawMessage `json:"reported,omitempty"`
+	Delta    json.RawMessage `json:"delta,omitempty"`
+}
+
+type thingDocumentRaw struct {
+	State     thingStateRaw `json:"state"`
+	Version   int           `json:"version,omitempty"`
+	Timestamp int           `json:"timestamp,omitempty"`
+}
+
+type thingDelta struct {
+	State     map[string]interface{} `json:"state"`
+	Version   int                    `json:"version,omitempty"`
+	Timestamp int                    `json:"timestamp,omitempty"`
+}
+
+func (s *ThingDocument) update(state *thingDocumentRaw) error {
+	if s.Version > state.Version {
+		// Received an old version; just ignore it.
+		return nil
+	}
+	s.Version = state.Version
+	s.Timestamp = state.Timestamp
+	if err := updateStateRaw(s.State.Desired, state.State.Desired); err != nil {
+		return err
+	}
+	if err := updateStateRaw(s.State.Reported, state.State.Reported); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *ThingDocument) updateDelta(state *thingDelta) (bool, error) {
+	if s.Version > state.Version {
+		// Received an old version; just ignore it.
+		return false, nil
+	}
+	s.Version = state.Version
+	s.Timestamp = state.Timestamp
+	s.State.Delta = state.State
+	return true, nil
+}
+
+func updateStateRaw(state map[string]interface{}, update json.RawMessage) error {
+	if !hasUpdate(update) {
+		return nil
+	}
+	if update == nil {
+		for k := range state {
+			delete(state, k)
+		}
+		return nil
+	}
+	var u map[string]interface{}
+	if err := json.Unmarshal([]byte(update), &u); err != nil {
+		return err
+	}
+	return updateState(state, u)
+}
+
+func updateState(state map[string]interface{}, update map[string]interface{}) error {
+	if len(update) == 0 {
+		for k := range state {
+			delete(state, k)
+		}
+		return nil
+	}
+	for key, val := range update {
+		switch v := val.(type) {
+		case map[string]interface{}:
+			if s, ok := state[key].(map[string]interface{}); ok {
+				if err := updateState(s, v); err != nil {
+					return err
+				}
+			} else {
+				state[key] = v
+			}
+		case nil:
+			if _, ok := state[key]; ok {
+				delete(state, key)
+			}
+		default:
+			state[key] = v
+		}
+	}
+	return nil
+}
+
+func hasUpdate(s json.RawMessage) bool {
+	switch {
+	case len(s) == 0:
+		return false
+	default:
+		return true
+	}
+}
