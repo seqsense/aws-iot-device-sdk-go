aaa387870a5adc88c5787827e17b20bcfdb72704
Atsushi Watanabe <atsushi.w@ieee.org>
2020-04-26 21:28:48 +0900

    Add Device interface

--- awsiotdev_test.go
+++ awsiotdev_test.go
@@ -12,36 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package awsiotdev implements offline queueing and reconnecting features of MQTT protocol.
 package awsiotdev
 
 import (
+	"net"
+	"testing"
+
 	"github.com/at-wat/mqtt-go"
 )
 
-// Device is an AWS IoT device.
-type Device interface {
-	mqtt.Client
-	ThingName() string
-}
+func TestNew(t *testing.T) {
+	ca, cb := net.Pipe()
+	go func() {
+		b := make([]byte, 100)
+		if _, err := ca.Read(b); err != nil {
+			t.Error(err)
+			return
+		}
+		// Send CONNACK.
+		if _, err := ca.Write([]byte{
+			0x20, 0x02, 0x00, 0x00,
+		}); err != nil {
+			t.Error(err)
+			return
+		}
+	}()
 
-type device struct {
-	mqtt.Client
-	thingName string
-}
-
-// New creates AWS IoT device interface.
-func New(thingName string, dialer mqtt.Dialer, opts ...mqtt.ReconnectOption) (Device, error) {
-	cli, err := mqtt.NewReconnectClient(dialer, opts...)
+	const name = "thing_name1"
+	d, err := New(name, mqtt.DialerFunc(func() (mqtt.ClientCloser, error) {
+		return &mqtt.BaseClient{Transport: cb}, nil
+	}))
 	if err != nil {
-		return nil, err
+		t.Fatal(err)
 	}
-	return &device{
-		Client:    cli,
-		thingName: thingName,
-	}, nil
-}
 
-func (d *device) ThingName() string {
-	return d.thingName
+	if d.ThingName() != name {
+		t.Errorf("ThingName differs, expected: %s, got: %s", name, d.ThingName())
+	}
 }
