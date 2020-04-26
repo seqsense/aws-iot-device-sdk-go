abe7bac518659e1d44c640fc8c8b855b2bd947fc
Atsushi Watanabe <atsushi.w@ieee.org>
2020-04-26 21:36:50 +0900

    Simplify test

--- awsiotdev_test.go
+++ awsiotdev_test.go
@@ -15,32 +15,15 @@
 package awsiotdev
 
 import (
-	"net"
 	"testing"
 
 	"github.com/at-wat/mqtt-go"
 )
 
 func TestNew(t *testing.T) {
-	ca, cb := net.Pipe()
-	go func() {
-		b := make([]byte, 100)
-		if _, err := ca.Read(b); err != nil {
-			t.Error(err)
-			return
-		}
-		// Send CONNACK.
-		if _, err := ca.Write([]byte{
-			0x20, 0x02, 0x00, 0x00,
-		}); err != nil {
-			t.Error(err)
-			return
-		}
-	}()
-
 	const name = "thing_name1"
 	d, err := New(name, mqtt.DialerFunc(func() (mqtt.ClientCloser, error) {
-		return &mqtt.BaseClient{Transport: cb}, nil
+		return &mqtt.BaseClient{}, nil
 	}))
 	if err != nil {
 		t.Fatal(err)
